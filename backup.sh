3dd5944e646bf74a1ee074684066cb64f3943441
Jakub Majzlik <jakubmajzlik@gmail.com>
2025-01-18 19:00:27 +0100

    Backup 18:01:2025 19:00:27

--- backup.sh
+++ backup.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+function copyFolder() {
+  local sourceFolder=$1
+  local destinationFolder=$2
+
+  # Check if the source fodler exists
+  if [ ! -d "$sourceFolder" ]; then
+     echo "Source folder does not exist: $sourceFolder"
+     exit 1
+  fi
+
+  # Check if the destination directory exists
+  if [ ! -d "$destinationFolder" ]; then
+     echo "Destination directory does not exist: $destinationFolder, creating it now"
+     mkdir -p $destinationFolder
+  fi
+
+  rm -rf "$destinationFolder"
+  cp -R "$sourceFolder" "$destinationFolder"
+
+  if [ $? -eq 0 ]; then
+     echo "Folder $sourcefile copied successfully."
+  else
+      echo "Failed $sourcefile to copy the folder."
+     exit 2
+  fi
+}
 
 function copyFile() {
   local sourcefile=$1
@@ -30,8 +56,14 @@ function copyFile() {
 NIXOS_SOURCE_FILE="/etc/nixos/configuration.nix"
 NIXOS_DESTINATION_DIR="nixos/etc/nixos/"
 
+# NeoVim - NvChad
+NVIM_NVCHAD_SOURCE_DIR="$HOME/.config/nvim"
+NVIM_NVCHAD_DESTINATION_DIR="neovim/.config/nvim"
+
+
 # Copying files
 copyFile "$NIXOS_SOURCE_FILE" "$NIXOS_DESTINATION_DIR"
+copyFolder "$NVIM_NVCHAD_SOURCE_DIR" "$NVIM_NVCHAD_DESTINATION_DIR"
 
 # Publishing into Github
 git add .
--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -15,18 +15,6 @@ return {
   require "configs.lspconfig"
   end,
   },
-  {
-    "rust-lang/rust.vim",
-    ft = "rust",
-    init = function ()
-      vim.g.rustfmt_autosave = 1 
-    end
-  },
-  {
-    "mrcjkb/rustaceanvim",
-    version = "^4",
-    ft = { "rust" },
-  },
   -- {
   -- 	"williamboman/mason.nvim",
   -- 	opts = {
